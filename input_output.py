ac8bd893cfe802fe8a8477a2fcf68e81188cdf30
Anubhab Roy <anubhab.roy.101@gmail.com>
2026-05-07 18:49:43 +0530

    Input_output_start

---
+++ input_output.py
@@ -0,0 +1,26 @@
+import math
+# input
+name = input("Enter your name: ")  # this is string input (Default)
+age = int(input("Enter your age: "))  # this is integer input
+marks = float(input("Enter your marks: "))  # this is float input
+# output
+print("Name:", name)
+print("Age:", age)
+print("Marks:", marks)
+
+## Practice
+# 1. WAP to take two numbers as input and print their sum.
+
+print("Welcome to the sum calculator!")
+print("Enter two numbers to find their sum:")
+num1 = int(input("First number: "))
+num2 = int(input("Second number: "))
+print("The sum of", num1, "and", num2, "is", num1 + num2)
+
+# 2. WAP to find the area of a square given the length of its side as input.
+
+print("Welcome to the area calculator for a square!")
+a = int(input("Enter the length of the side of the square: "))
+print("The area of the square is (using multiplication):", a * a)
+print("The area of the square is (using exponentiation):", a ** 2)
+print("The area of the square is (using math.pow):", math.pow(a, 2))
