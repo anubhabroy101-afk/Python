d2600d1b2a222478c366f351e072842e44b5318a
Anubhab Roy <anubhab.roy.101@gmail.com>
2026-05-10 08:16:23 +0530

    NoteAdd functions for calculating Q)simple interest, Q)area and perimeter of square, area and perimeter of circle, and Q)leap year check

---
+++ aa.py
@@ -0,0 +1,12 @@
+def block1():
+    print("Running block 1")
+
+def block2():
+    print("Running block 2")
+
+def block3():
+    print("Running block 3")
+
+
+if __name__ == "__main__":
+    block2()   # Run only this
---
+++ notes/lec_1.py
@@ -0,0 +1,35 @@
+# WAP in Python to calculate simple interest
+def block1():
+    p = int(input("Enter the principal : "))
+    t = int(input("Enter the time period : "))
+    r = int(input("Enter the rate of interest : "))
+    i = (p * t * r) / 100
+    print("The simple interest is : ", i)
+
+# WAP in Pyhton to calculate the area and perimeter of a sqare
+def block2():
+    l=int(input("Enter the side of the square : "))
+    a=l*l
+    print("The area of the square is : ",a)
+    p=4*l
+    print("The perimeter of the square is : ",p)
+
+# WAP in python to a calculate the area and perimeter of a circle
+def block3():
+    import math
+    r=int(input("Enter the radius of the circle : "))
+    a=math.pi*pow(r,2)
+    p=2*math.pi*r
+    print("The area of the circle is : ",a)
+    print("The perimeter of the circle is : ",p)
+
+# WAP to check a leap year
+def block4():
+    y=int(input("Enter the year : "))
+    if (y%4==0 and y%100!=0) or (y%400==0): #Keep in mind that the indentation in python is very important
+        print("The year is a leap year")
+    else:
+        print("The year is not a leap year")
+
+if __name__ == "__main__":
+    block1()   # Run only this
