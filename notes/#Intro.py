af81471d5ddfc1878393f5bfba5e31b345c788f1
Anubhab Roy <anubhab.roy.101@gmail.com>
2026-05-10 08:12:42 +0530

    Note_intro done

--- notes/#Intro.py
+++ notes/#Intro.py
@@ -1,5 +1,18 @@
 #Datatype=>
 #1. int=>integer
 #2. float=>decimal number   
-#3. str=>string
+#3. str=>string (default)
 #4. bool=>boolean (True/False)
+
+a= int (input("Enter a number: "))
+b= int (input("Enter another number: "))
+c= a + b
+print("The sum of", a, "and", b, "is", c)
+d= a - b
+print("The difference of", a, "and", b, "is", d)
+e= a * b
+print("The product of", a, "and", b, "is", e)
+f= a / b
+print("The quotient of", a, "and", b, "is", f)
+g= a % b
+print("The remainder of", a, "and", b, "is", g)
