14adbb8ca176fc24c3edef7e198417c839bcfcf4
Anubhab Roy <anubhab.roy.101@gmail.com>
2026-05-13 00:53:05 +0530

    done again

--- class/quadratic1.py
+++ class/quadratic1.py
@@ -12,7 +12,7 @@ d = (b ** 2) - (4 * a * c)
 print("Discriminant =", d)
 
 # Check nature of roots
-if d > 0:
+if d > 0: #Eg: x²+5x+6=0
     # Two different real roots
     root1 = (-b + math.sqrt(d)) / (2 * a)
     root2 = (-b - math.sqrt(d)) / (2 * a)
@@ -20,14 +20,14 @@ if d > 0:
     print("Two distinct real roots")
     print("Roots are:", root1, root2)
 
-elif d == 0:
+elif d == 0:#Eg: x²+4x+4=0
     # Equal roots
     root = -b / (2 * a)
 
     print("Two equal roots")
     print("Root =", root)
 
-else:
+else:#Eg: x²+2x+5=0
     # Complex roots
     real_part = -b / (2 * a)
     imaginary_part = math.sqrt(-d) / (2 * a)
