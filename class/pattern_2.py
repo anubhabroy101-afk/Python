428b4816c70c8e4aff19b97427bc63cf74dbc444
Anubhab Roy <anubhab.roy.101@gmail.com>
2026-05-15 20:38:13 +0530

    Pattern_M2

--- class/pattern_2.py
+++ class/pattern_2.py
@@ -4,6 +4,7 @@
 #    * * *
 #  * * * *
 #* * * * *
+print("Method 1:")
 n = int(input("Enter the number of rows: "))
 for i in range (n):
     for j in range (2*(n-i-1)):
@@ -11,3 +12,7 @@ for i in range (n):
     for k in range (i+1):
         print(" *", end="")
     print()
+
+print("Method 2:")
+for i in range(1, n+1):
+    print(" " * (n - i) + "*" * i)
