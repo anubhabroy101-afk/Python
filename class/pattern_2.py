d25d2f2e211f8f206eabe59d78bbc10e5075f40a
Anubhab Roy <anubhab.roy.101@gmail.com>
2026-05-15 07:59:31 +0530

    Update pattern_2.py to include detailed comments for the printed pattern

--- class/pattern_2.py
+++ class/pattern_2.py
@@ -1,8 +1,9 @@
 #WAP in python to print the following pattern
-#    *
-#   **
-#  ***
-# ****
+#        *
+#      * *
+#    * * *
+#  * * * *
+#* * * * *
 n = int(input("Enter the number of rows: "))
 for i in range (n):
     for j in range (2*(n-i-1)):
