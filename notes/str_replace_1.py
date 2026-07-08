ebf0ec8b1cb62b1b996d7382455977bc1521f458
Anubhab Roy <anubhab.roy.101@gmail.com>
2026-07-08 18:19:33 +0530

    Add comments to clarify the purpose of string replacement scripts

--- notes/str_replace_1.py
+++ notes/str_replace_1.py
@@ -1,3 +1,5 @@
+#WAP in python to replace a character of a string with 'm' and print the new string.
+
 a = str(input("Enter a string: "))
 b = a.replace("An","m")
 #print(a.replace(01,"m"))
--- notes/str_replace_2.py
+++ notes/str_replace_2.py
@@ -1,3 +1,5 @@
+#WAP in python to replace a character of a string with 'm' and print the new string.
+
 a = str(input("Enter a string: "))
 b = a.replace(a[1:2],"m")
 print(b)
